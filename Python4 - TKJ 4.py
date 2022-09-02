372b9bfbec0ba25d47138c8da91725d7e6823ba6
prasetyohatmono <112526214+prasetyohatmono@users.noreply.github.com>
2022-09-02 14:25:36 +0700

    Add files via upload

---
+++ Python4 - TKJ 4.py
@@ -0,0 +1,40 @@
+# rumus persegi panjang panjang * lebar
+
+panjang = 10
+lebar = 20
+luas_pp = panjang * lebar
+
+print("luas persegi panjang adalah", luas_pp)
+
+# rumus Trapesium 1/2 * (a+b) * t
+print("\nTrapesium")
+a = 6
+b = 10
+t = 8
+luas_t = (a+b) * t / 2
+print("Luas Trapesium adalah", luas_t)
+
+# rumus segitiga 1/2 * a * t
+print("\nSegitiga")
+a = 10
+t = 15
+luas_s = a * t / 2
+print("Luas Segitiga adalah", luas_s)
+
+# rumus Jajar Genjang a * t
+print("\nJajarGenjang")
+
+a = 10
+t = 15
+luas_jg = a * t
+print("Luas Jajar Genjang", luas_jg)
+
+# rumus Lingkaran n * r * r * L
+print("\Lingkaran")
+
+phi = 3.14
+r = 5
+r = 10
+l = 15
+luas_l = phi * r * r * l
+print("luas Lingkaran", luas_l)
