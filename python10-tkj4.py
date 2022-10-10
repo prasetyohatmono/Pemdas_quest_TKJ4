f7f14c2b96a5f58a3f4c21dad79f563b2f8070ef
prasetyohatmono <112526214+prasetyohatmono@users.noreply.github.com>
2022-10-10 09:54:44 +0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,26 @@
+#membuat tipe data koleksi
+
+koleksi_data_str = ["pisang","mangga","jambu","semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["Rizky Billar", 100, True, "Reza Arap"]
+
+print("koleksi data string:", koleksi_data_str)
+
+print("koleksi data integer:", koleksi_data_int)
+
+print("koleksi data campuran:", koleksi_data_mix)
+
+
+koleksi_nama_hewan = ["Ubur ubur","Macan","Gurita","Cacing"]
+
+koleksi_data_int = [100, 100, 100, 100]
+
+koleksi_nama_teman = ["Rendra","Jamil","Rasyid Ridho","Opai"]
+
+print("koleksi data string:", koleksi_nama_hewan)
+
+print("koleksi data integer:", koleksi_data_int)
+
+print("koleksi data string:", koleksi_nama_teman)
