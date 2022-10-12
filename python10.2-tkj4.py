ec4dd3c51f2c3284276ee7c46115e6199d425abc
prasetyohatmono <112526214+prasetyohatmono@users.noreply.github.com>
2022-10-12 14:05:29 +0700

    Add files via upload

---
+++ python10.2-tkj4.py
@@ -0,0 +1,58 @@
+#membuat tipe data koleksi
+
+koleksi_data_str = ["pisang","mangga","jambu","semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["Rizky Billar", 100, True, "Reza Arap"]
+
+print("koleksi data string:", koleksi_data_str)
+
+print("koleksi data integer:", koleksi_data_int)
+
+print("koleksi data campuran:", koleksi_data_mix)
+
+
+koleksi_nama_hewan = ["Ubur ubur","Macan","Gurita","Cacing"]
+
+koleksi_data_int = [100, 100, 100, 100]
+
+koleksi_nama_teman = ["Rendra","Jamil","Rasyid Ridho","Opai"]
+
+print("koleksi data string:", koleksi_nama_hewan)
+
+print("koleksi data integer:", koleksi_data_int)
+
+print("koleksi data string:", koleksi_nama_teman)
+
+#tampilkan data koleksi dengan indeks
+
+#data ke 2 nama hewan
+
+print("Nama Hewan Data ke-2:", koleksi_nama_hewan[1])
+
+#data pertama nilai UTS 
+
+print("Nilai PTS Data ke-1:", koleksi_data_int[0])
+
+#data ke 3 nama teman sebangku
+
+print("Nama Teman Sebangku ke-3:", koleksi_nama_teman[2])
+
+#tambahkan 1 data disetiap data koleksi
+
+koleksi_nama_teman.append("Jamil Roblox")
+
+print(koleksi_nama_teman)
+
+#ubahlah Data Terakhir Nilai UTS
+
+koleksi_data_int[-1] = "50"
+
+print(koleksi_data_int)
+
+#ubahlah data ke-3 nama hewan
+
+koleksi_nama_hewan[2] = "onta"
+
+print(koleksi_nama_hewan)
