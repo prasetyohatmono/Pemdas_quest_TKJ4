1c96791bce163b40e54d09e5643c03609e1ece76
prasetyohatmono <112526214+prasetyohatmono@users.noreply.github.com>
2022-10-18 09:47:15 +0700

    Add files via upload

---
+++ Python11_XTKJ4.py
@@ -0,0 +1,17 @@
+#Data Nama Makanan
+
+nama_makanan = ("Martabak","Bakso","Siomay","Pizza")
+
+print(nama_makanan)
+
+#Data Nama Teman
+
+nama_teman = ("Jamil","Ade","Akmal","Rasyid")
+
+print(nama_teman)
+
+#Data Nama Hewan
+
+nama_hewan = ("Kucing","Sapi","Kambing","Kura - kura")
+
+print(nama_hewan)
